db29abedb69479b40babd74a47e45f5bc4942916
ObeadAlhadreti <62612034+ObeadAlhadreti@users.noreply.github.com>
2020-09-17 15:16:49 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,180 @@
+<?php
+include("headerindex.php");
+?>
+    <!--content-->
+    <script>
+    history.pushState(null, null, null);
+    window.addEventListener('popstate', function () {
+        history.pushState(null, null, null);
+    });
+</script>
+	
+	<div class="content">
+        <div class="">
+
+            <!-- Start Our Services -->
+            <div class="our-Services">
+                <div class="container">
+                    <div class="hed-h2">
+                        <h2>أهلاً وسهلاً بكم</h2>
+                        <div class="border-h2">
+                            <img src="img/border.png" class="img-responsive center-block">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="serviceBox">
+                                <div class="service-icon">
+                                    <span><i class="fa fa-newspaper-o"></i></span>
+                                </div>
+                                <div class="service-content">
+                                    <h3 class="title">آخر الأخبار </h3>
+                                    <div class="media">
+                                        <div class="media-right">
+                                            <a href="#">
+                                              <img class="media-object" src="img/port_small9.png" width="100" height="70" >
+                                            </a>
+                                        </div>
+                                        <div class="media-body">
+                                            <h4 class="media-heading text-right"><a href="#">تكريم الطالب محمد الراشدي المتدرب بمستشفى المظيلف العام على جديته و إنضباطه خلال فترة التدريب</a></h4>
+                                            <div class="post-detels text-right">
+                                                <i class="fa fa-calendar fa-fw"></i> ١٤٣٩/١٢/٨ هـ
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="media">
+                                        <div class="media-right">
+                                            <a href="#">
+                                              <img class="media-object" src="img/port_small8.png" width="100" height="70" >
+                                            </a>
+                                        </div>
+                                        <div class="media-body">
+                                            <h4 class="media-heading text-right"><a href="#"> بلغ عدد الطلاب المتدربين بهذا الفصل 197 طالباً موزعين على 54 جهة تدريبية
+                                                  </a></h4>
+                                            <div class="post-detels text-right">
+                                                <i class="fa fa-calendar fa-fw"></i> 13/١0/1440 هـ
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="media">
+                                        <div class="media-right">
+                                            <a href="#">
+                                              <img class="media-object" src="img/port_small8.png" width="100" height="70" >
+                                            </a>
+                                        </div>
+                                        <div class="media-body">
+                                            <h4 class="media-heading text-right"><a href="#"> وكالة الكلية للتدريب والعلاقات الصناعية تتمنى لكم فصلا تدريبياً ناجحاً وتنبه على أهمية رفع التقارير الأسبوعية عبر بوابة التدريب الصيفي</a></h4>
+                                            <div class="post-detels text-right">
+                                                <i class="fa fa-calendar fa-fw"></i> 13/١0/1440 هـ
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br>
+                                    <div class="clearfix"></div>
+                                    <div class="text-left"><a href="#" class="btn btn-link"> أرشيف  الأخبار </a></div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="serviceBox">
+
+                                <div class="service-icon">
+                                    <span>                                <i class="fa fa-calendar" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+                                <div class="service-content">
+                                    <h3 class="title"> أحداث التدريب</h3>
+                                    <div id="amazingcarousel-container-1">
+                                        <div id="amazingcarousel-1" style="display:none;position:relative;width:100%;max-width:500px;margin:0px auto 0px;">
+                                            <div class="amazingcarousel-list-container">
+                                                <ul class="amazingcarousel-list">
+                                                    <li class="amazingcarousel-item">
+                                                        <div class="amazingcarousel-item-container">
+                                                            <div class="amazingcarousel-image">
+                                                                <a href="#">
+                                                                    <div class="date-icon">
+                                                                        1441/07/01
+                                                                    </div>
+                                                                </a>
+                                                            </div>
+                                                            <div class="amazingcarousel-text">
+                                                                <div class="amazingcarousel-description">بداية تسجيل البيانات على بوابة التدريب</div>
+                                                            </div>
+                                                            <div style="clear:both;"></div>
+                                                        </div>
+                                                    </li>
+                                                    <li class="amazingcarousel-item">
+                                                        <div class="amazingcarousel-item-container">
+                                                            <div class="amazingcarousel-image">
+                                                                <a href="#">
+                                                                    <div class="date-icon">
+                                                                        1441/08/11
+                                                                    </div>
+                                                                </a>
+                                                            </div>
+                                                            <div class="amazingcarousel-text">
+                                                                <div class="amazingcarousel-description">اختيار الفرص التدريبية</div>
+                                                            </div>
+                                                            <div style="clear:both;"></div>
+                                                        </div>
+                                                    </li>
+                                                    <li class="amazingcarousel-item">
+                                                        <div class="amazingcarousel-item-container">
+                                                            <div class="amazingcarousel-image">
+                                                                <a href="#">
+                                                                    <div class="date-icon">
+                                                                        1441/09/10
+                                                                    </div>
+                                                                </a>
+                                                            </div>
+                                                            <div class="amazingcarousel-text">
+                                                                <div class="amazingcarousel-description">طباعة الخطابات وتسليمها للجهات التدريبية</div>
+                                                            </div>
+                                                            <div style="clear:both;"></div>
+                                                        </div>
+                                                    </li>
+                                                    <li class="amazingcarousel-item">
+                                                        <div class="amazingcarousel-item-container">
+                                                            <div class="amazingcarousel-image">
+                                                                <a href="#">
+                                                                    <div class="date-icon">
+                                                                        1441/10/08
+                                                                    </div>
+                                                                </a>
+                                                            </div>
+                                                            <div class="amazingcarousel-text">
+                                                                <div class="amazingcarousel-description"> بداية التدريب الصيفي 1440-1441</div>
+                                                            </div>
+                                                            <div style="clear:both;"></div>
+                                                        </div>
+                                                    </li>
+
+
+                                                </ul>
+                                                <div class="amazingcarousel-prev"></div>
+                                                <div class="amazingcarousel-next"></div>
+                                            </div>
+                                            <div class="amazingcarousel-nav"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+            <!-- End Our Services -->
+
+
+        </div>
+    </div>
+    <!--content-->
+<?php
+include("footer.php");
+?>
